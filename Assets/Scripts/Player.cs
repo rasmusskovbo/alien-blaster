44f52a89d360b0a8c0eecffe3e6a344c805f8285
Rasmus Skovbo <rasmus.skovbo@shape.dk>
2022-02-18 13:14:56 +0100

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,50 @@ using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float horizontalMoveSpeed;
-    [SerializeField] float verticalMoveSpeed;
+    [Header("Player Movement")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float padding;
     
-    private Vector2 rawInput;
     
     
-    void Update()
+    private Vector2 inputDirection;
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
+
+    private void Start()
     {
-        Vector3 delta = rawInput;
-        transform.position += delta;
+        initBounds();
     }
 
+    void Update()
+    {
+        UpdatePlayerPosition();
+    }
+
+    // Movement
     void OnMove(InputValue value)
     {
-        rawInput = value.Get<Vector2>();
+        inputDirection = value.Get<Vector2>();
     }
+    
+    private void UpdatePlayerPosition()
+    {
+        Vector3 delta = inputDirection * moveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+        
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + padding, maxBounds.x - padding);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + padding, maxBounds.y - padding);
+        
+        this.transform.position = newPosition;
+    }
+
+    void initBounds()
+    {
+        Camera mainCamera = Camera.main;
+
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+
 }
